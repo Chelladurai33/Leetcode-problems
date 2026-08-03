817f5690e7aebbb0997ee240d0e39d94e0c88abe
Chelladurai33 <chelladuraimoffl@gmail.com>
2026-08-03 22:32:00 +0530

    Time: 11 ms (8.26%), Space: 47 MB (99.97%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int arr[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            int count = 0;
+            String b = Integer.toBinaryString(i);
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == '1') {
+                    count++;
+                }
+            }
+            arr[i] = count; 
+        }
+        return arr;
+    }
+}
